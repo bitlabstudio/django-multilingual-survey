ac04b3c362243bad7d3f0ae7c972e55f4b7f0340
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2015-01-20 10:13:48 +0100

    also allow to filter by questions

--- multilingual_survey/templatetags/survey_tags.py
+++ multilingual_survey/templatetags/survey_tags.py
@@ -6,11 +6,11 @@ register = template.Library()
 
 
 @register.assignment_tag()
-def filter_responses(answer, user_selection, session_selection):
+def filter_responses(obj, user_selection, session_selection):
     """Returns the respons count for a specific user selection."""
     if user_selection or session_selection:
-        responses = answer.responses.filter(
-            user__pk__in=user_selection) | answer.responses.filter(
+        responses = obj.responses.filter(
+            user__pk__in=user_selection) | obj.responses.filter(
                 session_id__in=session_selection)
         return responses.distinct()
-    return answer.responses.all()
+    return obj.responses.all()
